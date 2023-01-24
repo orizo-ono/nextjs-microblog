38d5dead6d283d9dc031e886c98df10077f32f35
Hono <>
2023-01-24 22:16:36 +0900

    ### セクション5の24.まで完了

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,17 +1,19 @@
 import Head from "next/head";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
 
 const name = "Hiroki Ono";
 export const siteTitle = "Next.js blog";
 
 export const Layout = ({ children }) => {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
-        <img src="/images/mens_icon.png" alt="" />
-        <h1>{name}</h1>
+      <header className={styles.header}>
+        <img src="/images/mens_icon.png" alt="" className={utilStyles.borderCircle} />
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
       </header>
       <main>{children}</main>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,60 @@
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <section className={utilStyles.headingMd}>
+        <p>私はフロントエンドエンジニアです。好きな言語はJavascriptです。</p>
+      </section>
+
+      <section className={`${utilStyles.headingMd}${utilStyles.padding1px}`}>
+        <h2>🗒エンジニアのブログ</h2>
+        <div className={styles.grid}>
+          <article>
+            <Link href="/">
+              <img src="/images/thumbnail01.jpg" alt="" className={styles.thumbnailImage} />
+            </Link>
+            <Link href="/">
+              <p className={utilStyles.boldText}>あああああああああああああああああ</p>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>2023/01/24</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img src="/images/thumbnail01.jpg" alt="" className={styles.thumbnailImage} />
+            </Link>
+            <Link href="/">
+              <p className={utilStyles.boldText}>あああああああああああああああああ</p>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>2023/01/24</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img src="/images/thumbnail01.jpg" alt="" className={styles.thumbnailImage} />
+            </Link>
+            <Link href="/">
+              <p className={utilStyles.boldText}>あああああああああああああああああ</p>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>2023/01/24</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img src="/images/thumbnail01.jpg" alt="" className={styles.thumbnailImage} />
+            </Link>
+            <Link href="/">
+              <p className={utilStyles.boldText}>あああああああああああああああああ</p>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>2023/01/24</small>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  );
 }
